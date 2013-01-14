b2b5b4660bf64ca2c626a4b914b94f3c13b42dd7
Dmitri Nesteruk <dmitrinesteruk@gmail.com>
2013-01-14 20:42:43 +0400

    quiz updates

--- matlab/quiz/Quiz2.m
+++ matlab/quiz/Quiz2.m
@@ -8,15 +8,15 @@ qd = q0*(1+r/m) ^ (t*m);
 subs(qd,[q0,r,m,t],[100,0.04,2,5])
 
 %% Question 2
-% Present value is calculated as $Q_{0c}=Q_n \mathrm{e}^{rt}$
+% Present value is calculated as $Q_{0}=Q_n \mathrm{e}^{rt}$
 qc = q0*exp(r*t);
 q0c = qn*exp(-r*t);
 subs(q0c,[qn,r,t],[1000000,0.05,100])
 
-%% 3
+%% Question 3
 % This is a solution to
 % 
-% $$\frac{dQ}{dt} = rq+k$$
+% $$\frac{dQ}{dt} = rQ+k$$
 % 
 qa = dsolve('Dq = r*q + k','q(0)=q0','t');
 subs(qa,[q0,r,t,k],[25,0.06,60,5000])
